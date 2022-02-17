9e2695174f6654158e604e8cef3c0c8a48c10f17
Brandon Nuñez <brandon@gmail.com>
2022-02-17 16:42:07 -0500

    Actualizacion 2.0

--- js/app.js
+++ js/app.js
@@ -6,17 +6,15 @@ function obtenerDatos(){
     const url = "https://dog.ceo/api/breeds/image/random"
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then( dog => mostrarHTML(dog));
+        .then( dog => mostrarHTML(dog))
+        .catch(error => console.log(error))
 }
 
 
 function mostrarHTML(dog){
-    const picture = document.querySelector("#picture");
     const {message} = dog
-
     picture.innerHTML = `
         <h1 style="color: darkblue;">Dog</h1>
-        <img src="${message}" height="360" width="500"></img>
+        <img src="${message}" height="360" width="360"></img>
     `;
-
 }
